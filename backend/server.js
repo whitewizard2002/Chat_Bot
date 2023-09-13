af7cf30d7112dc478c3ea319fc4b2edd2c24a503
Aadit Harshal Baldha <aaditbaldha2002@gmail.com>
2023-09-13 22:21:30 +0530

    connection to backend done

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,14 @@ const url=process.env.URL;
 
 //check the update is applicable
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+    });
 app.listen(port,()=>{
     console.log("Server is up and listening at port: "+port);
     
@@ -30,6 +38,11 @@ app.listen(port,()=>{
 app.get("/",(req,res)=>{
     console.log("get / called");
 
+})
+
+app.post("/Login",(req,res)=>{
+    console.log("post method called for login");
+    console.log("Username:",req.body);
 })
 
 // var mongoClient=require('mongodb').MongoClient;
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,5 +1,6 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import '../css/components/loginForm.css';
+import axios from 'axios';
 function LoginForm(){
     // [username,setUsername]=useState("");
     // [password,setPassword]=useState("");
@@ -14,15 +15,22 @@ function LoginForm(){
     }
 
     const formSubmit=(event)=>{
-        
+        handleSubmit();
     }
+
+    async function handleSubmit(){
+        const data=await axios.post('http://localhost:3001/Login',inputs);
+
+    }
+
+    useEffect(()=>{handleSubmit();},[]);
 
     return <div class="form">
             <form onSubmit={formSubmit}>
                 <center><h1 class="formLabel">Sign In</h1></center>
                 <input type="text" name="username" required value={inputs.username} onChange={handleChange} placeholder='Username'/><br/>
                 <input type="password" name="password" required value={inputs.password} onChange={handleChange} placeholder='Password'/><br/>
-                <center><input type="submit" onClick={formSubmit}/></center>
+                <center><input type="submit"/></center>
             </form>
         </div>
 }
